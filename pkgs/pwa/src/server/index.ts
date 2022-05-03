afe437c1c2430be45f12349a67a27b9b148bd345
mebtte <hi@mebtte.com>
2022-05-03 14:32:40 +0800

    fix server error message

--- pkgs/pwa/src/server/index.ts
+++ pkgs/pwa/src/server/index.ts
@@ -72,11 +72,8 @@ function generateMethod(method: METHOD) {
     }
 
     const { status, statusText } = response;
-    if (status !== 200 && !response.data) {
-      throw new ErrorWithCode(
-        `${status === 404 ? '请升级客户端到最新版本' : statusText}(#${status})`,
-        status,
-      );
+    if (status !== 200 || !response.data) {
+      throw new ErrorWithCode(`${statusText}(#${status})`, status);
     }
 
     const {
