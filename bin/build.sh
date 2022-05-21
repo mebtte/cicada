d1274102a444d6e753a29ec2dc0a59d890ef382a
mebtte <hi@mebtte.com>
2022-05-21 21:40:36 +0800

    fix build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -d build ]; then
+  rm -rf build/*
+else
+  mkdir build
+fi
+
 npm i
 
 npm run build:pwa
 npm run build:server
 
-mkdir build
 cp -R apps/server/build/* build
 mkdir build/pwa
 cp -R apps/pwa/build/* build/pwa
