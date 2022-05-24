8c42b1c2d010385262930aa875bc9c56408bac22
mebtte <hi@mebtte.com>
2022-05-24 18:06:51 +0800

    send email when create user

--- apps/server/src/api/controllers/create_user.ts
+++ apps/server/src/api/controllers/create_user.ts
@@ -1,22 +1,43 @@
+import day from '#/utils/day';
 import { ExceptionCode } from '#/constants/exception';
 import { EMAIL } from '#/constants/regexp';
 import { Property, getUserByEmail } from '@/platform/user';
 import * as db from '@/platform/db';
 import generateRandomInteger from '#/utils/generate_random_integer';
 import { REMARK_MAX_LENGTH } from '#/constants/user';
+import { sendMail } from '@/platform/email';
+import argv from '@/argv';
 import { Context } from '../constants/koa';
 
+const generateEmailHtml = () => `Hi,
+<br>
+<br>
+已成功为您创建账号, 现在可以使用当前邮箱登录到「<a href="${
+  argv.publicAddress
+}">知了</a>」.
+<br>
+如果使用中有任何问题或者建议, 可以通过 <a href="https://github.com/mebtte/cicada">Issue</a> 进行反馈.
+<br>
+<br>
+知了 ${day(new Date()).format('YYYY-MM-DD HH:mm')}`;
+
 export default async (ctx: Context) => {
-  const { email, remark = '' } = ctx.body as {
+  const {
+    email,
+    remark = '',
+    super: isSuper,
+  } = ctx.request.body as {
     email?: string;
     remark?: string;
+    super?: 0 | 1;
   };
 
   if (
     typeof email !== 'string' ||
     !EMAIL.test(email) ||
     typeof remark !== 'string' ||
-    remark.length > REMARK_MAX_LENGTH
+    remark.length > REMARK_MAX_LENGTH ||
+    (isSuper !== 0 && isSuper !== 1)
   ) {
     return ctx.except(ExceptionCode.PARAMETER_ERROR);
   }
@@ -26,13 +47,19 @@ export default async (ctx: Context) => {
     return ctx.except(ExceptionCode.EMAIL_EXISTED);
   }
 
+  await sendMail({
+    to: email,
+    title: '欢迎使用知了',
+    html: generateEmailHtml(),
+  });
+
   const id = generateRandomInteger(1_0000_0000, 10_0000_0000).toString();
   await db.run(
     `
-      insert into user(id, email, nickname, joinTimestamp, remark)
-        values(?, ?, ?, ?, ?)
+      insert into user(id, email, nickname, joinTimestamp, remark, super)
+        values(?, ?, ?, ?, ?, ?)
     `,
-    [id, email, id, Date.now(), remark],
+    [id, email, id, Date.now(), remark, isSuper],
   );
 
   return ctx.success();
