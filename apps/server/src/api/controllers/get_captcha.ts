506604d93e9a7ed87881fd86e99a69ac3c2285b9
mebtte <hi@mebtte.com>
2022-08-10 17:12:24 +0800

    improve get captcha

--- apps/server/src/api/controllers/get_captcha.ts
+++ apps/server/src/api/controllers/get_captcha.ts
@@ -6,7 +6,7 @@ import { Context } from '../constants';
 export default async (ctx: Context) => {
   const captchaData = captcha.create({
     size: 5,
-    ignoreChars: '01oOiIlL',
+    ignoreChars: '0oO1IlL2zZ',
     noise: 4,
   });
   const id = generateRandomString(8, false);
