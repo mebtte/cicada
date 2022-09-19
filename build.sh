a90c21c924ef7f38879e21b3e068ee96e0135023
mebtte <hi@mebtte.com>
2022-09-19 12:23:29 +0800

    fix pkg

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ sqlite3_pkg=$(cat node_modules/sqlite3/package.json)
 sqlite3_version=$(node -e "console.log(JSON.parse(\`$sqlite3_pkg\`).version)")
 
 # # download sqlite3 binary that need to pkg
-targets=("napi-v3-darwin-unknown-arm64" "napi-v3-darwin-unknown-x64" "napi-v3-win32-unknown-x64" "napi-v3-linux-glibc-x64" "napi-v3-linux-glibc-arm64")
+targets=("napi-v6-darwin-unknown-arm64" "napi-v6-darwin-unknown-x64" "napi-v6-win32-unknown-x64" "napi-v6-linux-glibc-x64" "napi-v6-linux-glibc-arm64")
 for target in ${targets[@]}; do
   wget https://github.com/TryGhost/node-sqlite3/releases/download/v$sqlite3_version/$target.tar.gz
   tar -zxvf $target.tar.gz
