a38af85d89c16ee685b47cc221c70be67b61fe03
mebtte <hi@mebtte.com>
2022-09-27 20:09:00 +0800

    remove icon

--- shared/components/icon/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Close } from './icons/close';
-export { default as Success } from './icons/success';
-export { default as Warning } from './icons/warning';
-export { default as Prompt } from './icons/prompt';
-export { default as Selected } from './icons/selected';
