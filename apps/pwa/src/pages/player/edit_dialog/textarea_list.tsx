b2c584229a92e3e4fc6b76f11436d47d27ec5d5d
mebtte <hi@mebtte.com>
2022-10-31 22:27:20 +0800

    improve lyric edit

--- apps/pwa/src/pages/player/edit_dialog/textarea_list.tsx
+++ apps/pwa/src/pages/player/edit_dialog/textarea_list.tsx
@@ -3,8 +3,9 @@ import { ForwardedRef, forwardRef, useImperativeHandle, useState } from 'react';
 import styled from 'styled-components';
 import Button from '#/components/button';
 import IconButton from '#/components/icon_button';
-import { MdDelete } from 'react-icons/md';
+import { MdDelete, MdUploadFile } from 'react-icons/md';
 import { ComponentSize } from '#/constants/style';
+import selectFile from '#/utils/select_file';
 import { Ref, RenderProps } from './constants';
 
 const Style = styled.div`
@@ -43,6 +44,28 @@ function Wrapper(
     );
   const onDelete = (id: number) =>
     setValues((vs) => vs.filter((v) => v.id !== id));
+  const onOpenFile = (id: number) =>
+    selectFile({
+      acceptTypes: ['text/plain'],
+      onSelect: (file) => {
+        if (file) {
+          const reader = new window.FileReader();
+          reader.readAsText(file);
+          reader.addEventListener('load', () =>
+            setValues((vs) =>
+              vs.map((v) =>
+                v.id === id
+                  ? {
+                      ...v,
+                      content: (reader.result as string) || v.content,
+                    }
+                  : v,
+              ),
+            ),
+          );
+        }
+      },
+    });
 
   useImperativeHandle(ref, () => ({
     getValue: () => values.map((v) => v.content),
@@ -55,12 +78,20 @@ function Wrapper(
           key={value.id}
           label={`${label} ${index + 1}`}
           addon={
-            <IconButton
-              size={ComponentSize.SMALL}
-              onClick={() => onDelete(value.id)}
-            >
-              <MdDelete />
-            </IconButton>
+            <>
+              <IconButton
+                size={ComponentSize.SMALL}
+                onClick={() => onOpenFile(value.id)}
+              >
+                <MdUploadFile />
+              </IconButton>
+              <IconButton
+                size={ComponentSize.SMALL}
+                onClick={() => onDelete(value.id)}
+              >
+                <MdDelete />
+              </IconButton>
+            </>
           }
           textareaProps={{
             value: value.content,
