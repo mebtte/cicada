dd907ce16d07a580b988486c866c741e66f8c4e9
mebtte <hi@mebtte.com>
2023-01-04 23:12:27 +0800

    fix clean outdated file

--- apps/server/src/schedule/jobs/clean_outdated_file.ts
+++ apps/server/src/schedule/jobs/clean_outdated_file.ts
@@ -12,29 +12,29 @@ import {
 const readdirAsync = util.promisify(fs.readdir);
 const rmAsync = util.promisify(fs.rm);
 const statAsync = util.promisify(fs.stat);
-const DIRECTORIES: {
-  directory: string;
-  ttl: number;
-}[] = [
-  {
-    directory: getTrashDirectory(),
-    ttl: 1000 * 60 * 60 * 24 * 30,
-  },
-  {
-    directory: getLogDirectory(),
-    ttl: 1000 * 60 * 60 * 24 * 30,
-  },
-  {
-    directory: getDownloadDirectory(),
-    ttl: DOWNLOAD_TTL,
-  },
-  {
-    directory: getDBSnapshotDirectory(),
-    ttl: 1000 * 60 * 60 * 24 * 60,
-  },
-];
 
 async function cleanOutdatedFile() {
+  const DIRECTORIES: {
+    directory: string;
+    ttl: number;
+  }[] = [
+    {
+      directory: getTrashDirectory(),
+      ttl: 1000 * 60 * 60 * 24 * 30,
+    },
+    {
+      directory: getLogDirectory(),
+      ttl: 1000 * 60 * 60 * 24 * 30,
+    },
+    {
+      directory: getDownloadDirectory(),
+      ttl: DOWNLOAD_TTL,
+    },
+    {
+      directory: getDBSnapshotDirectory(),
+      ttl: 1000 * 60 * 60 * 24 * 60,
+    },
+  ];
   for (const { directory, ttl } of DIRECTORIES) {
     const files = await readdirAsync(directory);
     for (const file of files) {
