6fa48f000621e75c95d53c1dfa0d06b059c93733
mebtte <hi@mebtte.com>
2023-02-03 14:35:58 +0800

    fix network status style error

--- apps/pwa/src/app/network_status.tsx
+++ apps/pwa/src/app/network_status.tsx
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 import sm from '@/global_states/server_metadata';
 import { useEffect, useState } from 'react';
 import { CSSVariable } from '@/global_style';
-import useTitlebarArea from '@/utils/use_titlebar_area_rect';
+import useTitlebarAreaRect from '@/utils/use_titlebar_area_rect';
 import { flexCenter } from '@/style/flexbox';
+import useWindowWidth from '@/utils/use_window_width';
 
 const Style = styled.div`
   ${flexCenter}
@@ -11,10 +12,12 @@ const Style = styled.div`
   font-size: 12px;
   background-color: ${CSSVariable.COLOR_DANGEROUS};
   color: #fff;
+  white-space: nowrap;
 `;
 
 function NetworkStatus() {
-  const { height, left, right } = useTitlebarArea();
+  const windowWidth = useWindowWidth();
+  const { height, left, right } = useTitlebarAreaRect();
   const serverMetadata = sm.useState();
   const [offline, setOffline] = useState(false);
 
@@ -33,7 +36,11 @@ function NetworkStatus() {
   if (serverMetadata.lastUpdateError || offline) {
     return (
       <Style
-        style={{ height: height || 30, paddingLeft: left, paddingRight: right }}
+        style={{
+          height: height || 30,
+          paddingLeft: left,
+          paddingRight: windowWidth - right,
+        }}
       >
         暂时无法连接到服务, 部分功能受限
       </Style>
