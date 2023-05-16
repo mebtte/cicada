3dccc5d6d014d876eb52cf87b19df0ff7928b81e
mebtte <hi@mebtte.com>
2023-05-16 16:15:27 +0800

    fix get music play record

--- apps/server/src/api_app/controllers/get_music_play_record_list.ts
+++ apps/server/src/api_app/controllers/get_music_play_record_list.ts
@@ -1,15 +1,22 @@
-import { ALIAS_DIVIDER } from '#/constants';
+import { ALIAS_DIVIDER, AssetType } from '#/constants';
 import { ExceptionCode } from '#/constants/exception';
 import { SEARCH_KEYWORD_MAX_LENGTH } from '#/constants/music';
 import excludeProperty from '#/utils/exclude_property';
 import {
+  MUSIC_PLAY_RECORD_TABLE_NAME,
+  MUSIC_SINGER_RELATION_TABLE_NAME,
+  MUSIC_TABLE_NAME,
   Music,
+  MusicPlayRecordProperty,
   MusicProperty,
+  MusicSingerRelationProperty,
+  SINGER_TABLE_NAME,
   Singer,
   SingerProperty,
 } from '@/constants/db_definition';
 import { getDB } from '@/db';
 import { getSingerListInMusicIds } from '@/db/singer';
+import { getAssetPublicPath } from '@/platform/asset';
 import { Context } from '../constants';
 
 const MAX_PAGE_SIZE = 100;
@@ -19,6 +26,8 @@ type LocalMusic = Pick<
   | MusicProperty.TYPE
   | MusicProperty.NAME
   | MusicProperty.ALIASES
+  | MusicProperty.COVER
+  | MusicProperty.ASSET
 >;
 type LocalMusicPlayRecord = LocalMusic & {
   recordId: number;
@@ -49,34 +58,34 @@ export default async (ctx: Context) => {
     const pattern = `%${keyword}%`;
     const musicPatternSQL = `
       SELECT
-        id
-      FROM music
-      WHERE name LIKE ?
-        OR aliases LIKE ?
+        ${MusicProperty.ID}
+      FROM ${MUSIC_TABLE_NAME}
+      WHERE ${MusicProperty.NAME} LIKE ?
+        OR ${MusicProperty.ALIASES} LIKE ?
     `;
     const singerPatternSQL = `
       SELECT
-        msr.musicId
-      FROM music_singer_relation AS msr
-      LEFT JOIN singer as s
-        ON msr.singerId = s.id
-      LEFT JOIN music as m
-        ON msr.musicId = m.id
-      WHERE s.name LIKE ?
-        OR s.aliases LIKE ?
+        msr.${MusicSingerRelationProperty.MUSIC_ID}
+      FROM ${MUSIC_SINGER_RELATION_TABLE_NAME} AS msr
+      LEFT JOIN ${SINGER_TABLE_NAME} as s
+        ON msr.${MusicSingerRelationProperty.SINGER_ID} = s.${SingerProperty.ID}
+      LEFT JOIN ${MUSIC_TABLE_NAME} as m
+        ON msr.${MusicSingerRelationProperty.MUSIC_ID} = m.${MusicProperty.ID}
+      WHERE s.${SingerProperty.NAME} LIKE ?
+        OR s.${SingerProperty.ALIASES} LIKE ?
     `;
     const [totalObject, localMusicPlayRecordList] = await Promise.all([
       getDB().get<{ value: number }>(
         `
           SELECT
             count(*) AS value
-          FROM music_play_record AS mpr
-          LEFT JOIN music AS m
-            ON mpr.musicId = m.id
-          WHERE mpr.userId = ?
+          FROM ${MUSIC_PLAY_RECORD_TABLE_NAME} AS mpr
+          LEFT JOIN ${MUSIC_TABLE_NAME} AS m
+            ON mpr.${MusicPlayRecordProperty.MUSIC_ID} = m.${MusicProperty.ID}
+          WHERE mpr.${MusicPlayRecordProperty.USER_ID} = ?
             AND (
-              m.id IN (${musicPatternSQL})
-              OR m.id IN (${singerPatternSQL})
+              m.${MusicProperty.ID} IN (${musicPatternSQL})
+              OR m.${MusicProperty.ID} IN (${singerPatternSQL})
             )
         `,
         [ctx.user.id, pattern, pattern, pattern, pattern],
@@ -84,27 +93,25 @@ export default async (ctx: Context) => {
       getDB().all<LocalMusicPlayRecord>(
         `
           SELECT
-            mpr.id as recordId,
-            mpr.percent,
-            mpr.timestamp,
+            mpr.${MusicPlayRecordProperty.ID} as recordId,
+            mpr.${MusicPlayRecordProperty.PERCENT},
+            mpr.${MusicPlayRecordProperty.TIMESTAMP},
             
-            m.id,
-            m.cover,
-            m.type,
-            m.name,
-            m.aliases,
-            m.sq,
-            m.hq,
-            m.ac
-          FROM music_play_record AS mpr
-          LEFT JOIN music AS m
-            ON mpr.musicId = m.id
-          WHERE mpr.userId = ?
+            m.${MusicProperty.ID},
+            m.${MusicProperty.COVER},
+            m.${MusicProperty.TYPE},
+            m.${MusicProperty.NAME},
+            m.${MusicProperty.ALIASES},
+            m.${MusicProperty.ASSET}
+          FROM ${MUSIC_PLAY_RECORD_TABLE_NAME} AS mpr
+          LEFT JOIN ${MUSIC_TABLE_NAME} AS m
+            ON mpr.${MusicPlayRecordProperty.MUSIC_ID} = m.${MusicProperty.ID}
+          WHERE mpr.${MusicPlayRecordProperty.USER_ID} = ?
             AND (
-              m.id IN (${musicPatternSQL})
-              OR m.id IN (${singerPatternSQL})
+              m.${MusicProperty.ID} IN (${musicPatternSQL})
+              OR m.${MusicProperty.ID} IN (${singerPatternSQL})
             )
-          ORDER BY mpr.timestamp DESC
+          ORDER BY mpr.${MusicPlayRecordProperty.TIMESTAMP} DESC
           LIMIT ?
           OFFSET ?
         `,
@@ -127,31 +134,29 @@ export default async (ctx: Context) => {
         `
           SELECT
             count(*) AS value
-          FROM music_play_record
-          WHERE userId = ?
+          FROM ${MUSIC_PLAY_RECORD_TABLE_NAME}
+          WHERE ${MusicPlayRecordProperty.USER_ID} = ?
         `,
         [ctx.user.id],
       ),
       getDB().all<LocalMusicPlayRecord>(
         `
           SELECT
-            mpr.id as recordId,
-            mpr.percent,
-            mpr.timestamp,
+            mpr.${MusicPlayRecordProperty.ID} as recordId,
+            mpr.${MusicPlayRecordProperty.PERCENT},
+            mpr.${MusicPlayRecordProperty.TIMESTAMP},
             
-            m.id,
-            m.cover,
-            m.type,
-            m.name,
-            m.aliases,
-            m.sq,
-            m.hq,
-            m.ac
-          FROM music_play_record AS mpr
-          LEFT JOIN music AS m
-            ON mpr.musicId = m.id
-          WHERE mpr.userId = ?
-          ORDER BY mpr.timestamp DESC
+            m.${MusicProperty.ID},
+            m.${MusicProperty.COVER},
+            m.${MusicProperty.TYPE},
+            m.${MusicProperty.NAME},
+            m.${MusicProperty.ALIASES},
+            m.${MusicProperty.ASSET}
+          FROM ${MUSIC_PLAY_RECORD_TABLE_NAME} AS mpr
+          LEFT JOIN ${MUSIC_TABLE_NAME} AS m
+            ON mpr.${MusicPlayRecordProperty.MUSIC_ID} = m.${MusicProperty.ID}
+          WHERE mpr.${MusicPlayRecordProperty.USER_ID} = ?
+          ORDER BY mpr.${MusicPlayRecordProperty.TIMESTAMP} DESC
           LIMIT ?
           OFFSET ?
         `,
@@ -171,27 +176,24 @@ export default async (ctx: Context) => {
 
   const singerList = await getSingerListInMusicIds(
     musicPlayRecordList.map((m) => m.id),
-    [SingerProperty.ID, SingerProperty.NAME, SingerProperty.ALIASES],
+    [SingerProperty.ID, SingerProperty.NAME],
   );
   const musicIdMapSingerList: {
-    [key: string]: (Pick<Singer, SingerProperty.ID | SingerProperty.NAME> & {
-      aliases: string[];
-    })[];
+    [key: string]: Pick<Singer, SingerProperty.ID | SingerProperty.NAME>[];
   } = {};
   singerList.forEach((s) => {
     if (!musicIdMapSingerList[s.musicId]) {
       musicIdMapSingerList[s.musicId] = [];
     }
-    musicIdMapSingerList[s.musicId].push({
-      ...excludeProperty(s, ['musicId']),
-      aliases: s.aliases ? s.aliases.split(ALIAS_DIVIDER) : [],
-    });
+    musicIdMapSingerList[s.musicId].push(excludeProperty(s, ['musicId']));
   });
 
   return ctx.success({
     total,
     musicPlayRecordList: musicPlayRecordList.map((m) => ({
       ...m,
+      cover: getAssetPublicPath(m.cover, AssetType.MUSIC_COVER),
+      asset: getAssetPublicPath(m.asset, AssetType.MUSIC),
       aliases: m.aliases ? m.aliases.split(ALIAS_DIVIDER) : [],
       singers: musicIdMapSingerList[m.id] || [],
     })),
