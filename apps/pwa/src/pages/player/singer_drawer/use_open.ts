38f720c0c161c0d936f6edf3ebcc4c12ef09b09c
mebtte <hi@mebtte.com>
2023-05-16 15:07:09 +0800

    improve music drawer

--- apps/pwa/src/pages/player/singer_drawer/use_open.ts
+++ apps/pwa/src/pages/player/singer_drawer/use_open.ts
@@ -1,22 +1,43 @@
 import { useCallback, useEffect, useState } from 'react';
-import useDynamicZIndex from '../use_dynamic_z_index';
+import useNavigate from '@/utils/use_navigate';
+import { Query } from '@/constants';
+import useQuery from '@/utils/use_query';
 import e, { EventType } from '../eventemitter';
+import useDynamicZIndex from '../use_dynamic_z_index';
 
 export default () => {
-  const zIndex = useDynamicZIndex(EventType.OPEN_SINGER_DRAWER);
-  // const [open, setOpen] = useState(true);
-  const [open, setOpen] = useState(false);
-  const onClose = useCallback(() => setOpen(false), []);
-  // const [singerId, setSingerId] = useState('vaffchhj');
-  const [singerId, setSingerId] = useState('');
+  const navigate = useNavigate();
+  const onClose = useCallback(
+    () =>
+      navigate({
+        query: {
+          [Query.SINGER_DRAWER_ID]: '',
+        },
+      }),
+    [navigate],
+  );
+  const { singer_drawer_id: urlId } = useQuery<Query.SINGER_DRAWER_ID>();
+  const [id, setId] = useState(urlId);
 
   useEffect(() => {
-    const unlistenOpen = e.listen(EventType.OPEN_SINGER_DRAWER, ({ id }) => {
-      setSingerId(id);
-      window.setTimeout(() => setOpen(true), 0);
-    });
-    return unlistenOpen;
-  }, []);
+    setId((i) => urlId || i);
+  }, [urlId]);
 
-  return { zIndex, singerId, open, onClose };
+  useEffect(() => {
+    const unlistenOpen = e.listen(EventType.OPEN_SINGER_DRAWER, (data) =>
+      navigate({
+        query: {
+          [Query.SINGER_DRAWER_ID]: data.id,
+        },
+      }),
+    );
+    return unlistenOpen;
+  }, [navigate]);
+
+  return {
+    zIndex: useDynamicZIndex(EventType.OPEN_SINGER_DRAWER),
+    id,
+    open: !!urlId,
+    onClose,
+  };
 };
