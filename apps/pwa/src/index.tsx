3a0497717b0563c773637a7f0a364625a575bb99
mebtte <hi@mebtte.com>
2023-10-17 12:34:15 +0800

    add devtool

--- apps/pwa/src/index.tsx
+++ apps/pwa/src/index.tsx
@@ -1,5 +1,6 @@
 import './polyfill'; // 需要保证 polyfill 在第一个
 import './updater';
+import './devtool';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import 'cropperjs/dist/cropper.min.css';
