d6ae6af9c3fb1d255dc2e10d25758041ff679ba0
mebtte <i@mebtte.com>
2025-11-18 23:18:59 +0800

    playlist view

--- apps/flutter/lib/player_controller/playlist.dart
+++ apps/flutter/lib/player_controller/playlist.dart
@@ -1,10 +1,35 @@
+import 'package:cicada/event_bus.dart';
+import 'package:cicada/states/playlist.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Playlist extends StatelessWidget {
   const Playlist({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("playlist");
+    final playlist = context.watch<PlaylistState>().playlist;
+    return playlist.isEmpty
+        ? Text("Playlist is empty")
+        : Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: playlist.length,
+                  itemBuilder: (context, index) {
+                    final playlistMusic = playlist[index];
+                    return ListTile(
+                      leading: const Icon(Icons.music_note_outlined),
+                      title: Text(playlistMusic.music.name),
+                      onTap: () => eventBus.fire(
+                        PlayMusicEvent(music: playlistMusic.music),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Text("action"),
+            ],
+          );
   }
 }
