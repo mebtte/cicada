0ac28bdbacf74add38ab040d3f25d1feaf25f22a
anyone <anyone@mebtte.com>
2026-05-12 15:00:20 +0800

    drawer, no page

--- apps/pwa/src/pages/player/singer_drawer/index.tsx
+++ apps/pwa/src/pages/player/singer_drawer/index.tsx
@@ -4,10 +4,10 @@ import useDynamicZIndex from '../use_dynamic_z_index';
 import { EventType } from '../eventemitter';
 
 function Wrapper() {
-  const { id, open, onClose, miniMode } = useOpen();
+  const { id, open, onClose } = useOpen();
   const zIndex = useDynamicZIndex(EventType.OPEN_SINGER_DRAWER);
 
-  if (!id || miniMode) {
+  if (!id) {
     return null;
   }
   return <SingerDrawer open={open} onClose={onClose} id={id} zIndex={zIndex} />;
--- apps/pwa/src/pages/player/singer_drawer/use_open.ts
+++ apps/pwa/src/pages/player/singer_drawer/use_open.ts
@@ -1,19 +1,11 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useNavigate as useRouterNavigate } from 'react-router-dom';
 import useNavigate from '@/utils/use_navigate';
 import { Query } from '@/constants';
-import { PLAYER_PATH, ROOT_PATH } from '@/constants/route';
 import useQuery from '@/utils/use_query';
-import { useTheme } from '@/global_states/theme';
 import e, { EventType } from '../eventemitter';
-
-const getSingerPath = (id: string) =>
-  `${ROOT_PATH.PLAYER}${PLAYER_PATH.SINGER.replace(':id', id)}`;
 
 export default () => {
   const navigate = useNavigate();
-  const routerNavigate = useRouterNavigate();
-  const { miniMode } = useTheme();
   const onClose = useCallback(
     () =>
       navigate({
@@ -31,35 +23,23 @@ export default () => {
   }, [urlId]);
 
   useEffect(() => {
-    if (miniMode && urlId) {
-      routerNavigate(getSingerPath(urlId), { replace: true });
-    }
-  }, [miniMode, routerNavigate, urlId]);
-
-  useEffect(() => {
     const unlistenOpen = e.listen(EventType.OPEN_SINGER_DRAWER, (data) =>
       window.setTimeout(
-        () => {
-          if (miniMode) {
-            routerNavigate(getSingerPath(data.id));
-            return;
-          }
+        () =>
           navigate({
             query: {
               [Query.SINGER_DRAWER_ID]: data.id,
             },
-          });
-        },
+          }),
         0,
       ),
     );
     return unlistenOpen;
-  }, [miniMode, navigate, routerNavigate]);
+  }, [navigate]);
 
   return {
     id,
-    miniMode,
-    open: !miniMode && !!urlId,
+    open: !!urlId,
     onClose,
   };
 };
